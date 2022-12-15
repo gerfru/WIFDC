ff25bbb89e374e91170714b160a2db44e3ec5c1e
Gerald Fruhmann <geraldfruhmann@gmail.com>
2022-12-15 16:57:16 +0100

    Comments & Documentation

--- Frontend/WIFDC.py
+++ Frontend/WIFDC.py
@@ -1,17 +1,9 @@
 from flask import Flask, render_template, abort, request, url_for, flash, redirect
-import datetime
 
+# Wir definieren die App und einen Namen, der vom Filenamen übernommen wird.
+# The flash() function stores flashed messages in the client’s browser session, which requires setting a secret key. This secret key is used to secure sessions, which allow Flask to remember information from one request to another, such as moving from the new message page to the index page. The user can access the information stored in the session, but cannot modify it unless they have the secret key, so you must never allow anyone to access your secret key. See the Flask documentation for sessions for more information
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '3a6be9453b5623d2c6ae4789271af440554380051de68c47'
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_error(error):
-    return render_template('500.html'), 500
-
 
 messages = [{'title': 'Nachricht 1',
              'content': 'Lena'},
@@ -19,31 +11,48 @@ messages = [{'title': 'Nachricht 1',
              'content': 'Lumos'}
             ]
 
+
+ # You use the @app.route() decorator to create a view function called index(), which calls the render_template() function as the return value, which in turn renders a template called index.html
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Here you use the @app.errorhandler() decorator to register the function page_not_found() as a custom error handler. The function takes the error as an argument, and it returns a call to the render_template() function with a template called 404.html. You also return the integer 404 after the render_template() call. This tells Flask that the status code in the response should be 404. If you don’t add it, the default status code response will be 200, which means that the request has succeeded.
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
+
+# Here you use the same pattern as you did for the 404 error handler. You use the app.errorhandler() decorator with a 500 argument to make a function called internal_error() into an error handler. You render a template called 500.html, and respond with a status code of 500.
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html'), 500
+
+# You use the @app.route() decorator to create a view function called Webform(), which calls the render_template() function as the return value, which in turn renders a template called Webform.html. In the Webform, messages will be displayed.
+# Furthermore, through parsing "messages", we display these messages on the site.
 @app.route('/Webform')
 def Webform():
     return render_template('Webform.html', messages=messages)
 
-@app.route('/500')
-def error500():
-    abort(500)
-
+# Too easy :-)
 @app.route('/about/')
 def about():
     return render_template('about.html')
 
-@app.route('/comments/')
-def comments():
-    comments = ['This is the first comment.',
-                'This is the second comment.',
-                'This is the third comment.',
-                'This is the fourth comment.'
-                ]
-    return render_template('comments.html', comments=comments)
+#@app.route('/comments/')
+#def comments():
+#    comments = ['This is the first comment.',
+#                'This is the second comment.',
+#                'This is the third comment.',
+#                'This is the fourth comment.'
+#                ]
+#    return render_template('comments.html', comments=comments)
 
+
+#In the route below, you have a URL variable idx. This is the index that will determine what message will be displayed. For example, if the URL is /messages/0, the first message (Message Zero) will be displayed. You use the int converter to accept only positive integers, because URL variables have string values by default.
+#
+#Inside the message() view function, you have a regular Python list called messages with three messages. (In a real-world scenario, these messages would come from a database, an API, or another external data source.) The function returns a call to the render_template() function with two arguments, message.html as the template file, and a message variable that will be passed to the template. This variable will have a list item from the messages list depending on the value of the idx variable in the URL.
+#
+# Additionally, we import the abort() function, which you use to abort the request and respond with an error. In the message() view function, you use a try ... except clause to wrap the function. You first try to return the messages template with the message that corresponds to the index in the URL. If the index has no corresponding message, the IndexError exception will be raised. You then use the except clause to catch that error, and you use abort(404) to abort the request and respond with a 404 Not Found HTTP error.
 @app.route('/messages/<int:idx>')
 def message(idx):
     app.logger.info('Building the messages list...')
@@ -56,6 +65,11 @@ def message(idx):
         abort(404)
 
 
+# In the if statement you ensure that the code following it is only executed when the request is a POST request via the comparison request.method == 'POST'.
+
+# You then extract the submitted title and content from the request.form object that gives you access to the form data in the request. If the title is not provided, the condition if not title would be fulfilled. In that case, you display a message to the user informing them that the title is required using the flash() function. This adds the message to a flashed messages list. You will later display these messages on the page as part of the base.html template. Similarly, if the content is not provided, the condition elif not content will be fulfilled. If so, you add the 'Content is required!' message to the list of flashed messages.
+
+# If the title and the content of the message are properly submitted, you use the line messages.append({'title': title, 'content': content}) to add a new dictionary to the messages list, with the title and content the user provided. Then you use the redirect() function to redirect users to the index page. You use the url_for() function to link to the index page.
 @app.route('/create/', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
